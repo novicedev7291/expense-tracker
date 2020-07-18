90c65be90de7367877061dcb476b1e0a06262720
kuldeep <kuldeepyadav7291@gmail.com>
2020-07-18 19:34:14 +0530

    Remove vaadin lib dependency
    Related: #PER-43

--- src/main/java/com/expense/tracker/adapter/web/ExpenseDto.java
+++ src/main/java/com/expense/tracker/adapter/web/ExpenseDto.java
@@ -2,9 +2,6 @@ package com.expense.tracker.adapter.web;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-
 /**
  * @author <a href="kuldeepyadav7291@gmail.com">Kuldeep</a>
  */
@@ -12,10 +9,7 @@ import javax.validation.constraints.NotNull;
 public class ExpenseDto {
     private Long id;
     private String category;
-    @NotEmpty
     private String description;
-    @NotNull
     private String amount;
-    @NotNull
     private String addedOn;
 }
--- src/main/java/com/expense/tracker/adapter/web/SheetController.java
+++ src/main/java/com/expense/tracker/adapter/web/SheetController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
  * @author <a href="kuldeepyadav7291@gmail.com">Kuldeep</a>
  */
 @RestController
+@RequestMapping("/api")
 public class SheetController {
     private final ExpenseSheetService service;
 
